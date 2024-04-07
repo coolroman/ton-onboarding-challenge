a8b90454b3ac7b1231e41148c1a7fe3198c69e6d
Roman Kulinsky <kulinsky.roman@gmail.com>
2024-04-07 11:55:35 +0300

    Preparing Mining Messages

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import { getHttpEndpoint } from "@orbs-network/ton-access";
 import { Address, TonClient, TupleItem } from "@ton/ton";
 import { BN } from "bn.js";
 import dotenvFlow from "dotenv-flow";
+import { Address as AddressTon } from "ton";
+import { MineMessageParams, Queries } from "./src/giver/NftGiver.data";
+import { unixNow } from "./src/lib/utils";
 
 dotenvFlow.config({ debug: false });
 
@@ -42,6 +45,44 @@ async function main() {
   console.log("target_delta", target_delta.toString());
   console.log("min_cpl", min_cpl.toString());
   console.log("max_cpl", max_cpl.toString());
+
+  const mineParams: MineMessageParams = {
+    expire: unixNow() + 300, // 5 min is enough to make a transaction
+    mintTo: AddressTon.parse(process.env.SC_ADDRESS!), // your wallet
+    data1: new BN(0), // temp variable to increment in the miner
+    seed, // unique seed from get_mining_data
+  };
+
+  let msg = Queries.mine(mineParams); // transaction builder
+
+  let progress = 0;
+
+  while (new BN(msg.hash(), "be").gt(complexity)) {
+    progress += 1;
+    console.clear();
+    console.log(
+      `Mining started: please, wait for 30-60 seconds to mine your NFT!`
+    );
+    console.log(" ");
+    console.log(
+      `⛏ Mined ${progress} hashes! Last: `,
+      new BN(msg.hash(), "be").toString()
+    );
+
+    mineParams.expire = unixNow() + 300;
+    mineParams.data1.iaddn(1);
+    msg = Queries.mine(mineParams);
+  }
+
+  console.log(" ");
+  console.log("💎 Mission completed: msg_hash less than pow_complexity found!");
+  console.log(" ");
+  console.log("msg_hash: ", new BN(msg.hash(), "be").toString());
+  console.log("pow_complexity: ", complexity.toString());
+  console.log(
+    "msg_hash < pow_complexity: ",
+    new BN(msg.hash(), "be").lt(complexity)
+  );
 }
 
 main();
